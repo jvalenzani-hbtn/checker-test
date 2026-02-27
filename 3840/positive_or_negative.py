605151ad42d1c63c30b32227913c189eda177571
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2026-02-27 10:57:31 -0300

    Change shebang and import method in script

--- 3840/positive_or_negative.py
+++ 3840/positive_or_negative.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
+#!/usr/bin/env python3
+number = __import__('random').randint(-10, 10)
 if number > 0:
     print("{} is positive".format(number))
 elif number == 0:
