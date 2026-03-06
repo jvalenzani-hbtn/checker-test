054e7b23ff726c06b6ad6d9a467f5380662b06a1
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2026-03-05 21:13:09 -0300

    Fix docstring formatting in Square class

--- 3847/0-square.py
+++ 3847/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-'''This is the Square Class Module'''
+'''This is the Square Class Module
+	Defines class Square
+'''
 
 class Square():
-'''This is the Square Class'''
+	'''This is the Square Class'''
 	pass
 
